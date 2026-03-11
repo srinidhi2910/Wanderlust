ff7a5df1959857c2172e71d508564b531d0a4ce6
Ponnala Srinidhi Reddy <srinidhireddy.cbit@gmail.com>
2026-03-12 00:11:09 +0530

    fixed tax toggle layout

--- schema.js
+++ schema.js
@@ -9,7 +9,8 @@ module.exports.listingSchema =Joi.object({
         location:Joi.string().required(),
         country:Joi.string().required(),
         price:Joi.number().required().min(0),
-        image:Joi.string().allow("",null)
+        image:Joi.string().allow("",null),
+        category: Joi.string().required()
     }).required()
 });
 
